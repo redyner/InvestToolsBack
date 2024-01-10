dcb876e6b2787c4d408ca006e9f996dda823f3f7
Rediner <rediner@hotmail.com>
2024-01-10 20:44:59 -0300

    Aportes adicionados ao Front

---
+++ FrontInvestTools/src/app/Enums/tipo-periodo.ts
@@ -0,0 +1,4 @@
+export enum tipoPeriodo{
+    mes = 1,
+    ano = 2
+}
--- FrontInvestTools/src/app/calculadora-juros/calculadora-juros.component.ts
+++ FrontInvestTools/src/app/calculadora-juros/calculadora-juros.component.ts
@@ -1,23 +1,68 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Validators, FormBuilder, FormArray } from '@angular/forms';
+import { tipoPeriodo } from '../Enums/tipo-periodo';
+import { mes } from '../Enums/mes';
 
 @Component({
   selector: 'app-calculadora-juros',
   templateUrl: './calculadora-juros.component.html',
-  styleUrl: './calculadora-juros.component.scss'
+  styleUrls: ['./calculadora-juros.component.scss']
 })
+
 export class CalculadoraJurosComponent {
-  form = new FormGroup({
-    inicial: new FormControl(0.00),
-    aporte: new FormControl('',Validators.required),
-    periodo: new FormControl('',Validators.required),
-    tipoPeriodoPeriodo: new FormControl('1',Validators.required),
-    juros: new FormControl('',Validators.required),
-    tipoPeriodoJuros: new FormControl('1',Validators.required),
+
+  meses = [
+    { value: 0, name: 'Todos' },
+    { value: 1, name: 'Janeiro' },
+    { value: 2, name: 'Fevereiro' },
+    { value: 3, name: 'Março' },
+    { value: 4, name: 'Abril' },
+    { value: 5, name: 'Maio' },
+    { value: 6, name: 'Junho' },
+    { value: 7, name: 'Julho' },
+    { value: 8, name: 'Agosto' },
+    { value: 9, name: 'Setembro' },
+    { value: 10, name: 'Outubro' },
+    { value: 11, name: 'Novembro' },
+    { value: 12, name: 'Dezembro' }
+  ];
+
+  form = this.formBuilder.group({
+    inicial: [0],
+    periodo: this.formBuilder.group({
+      valor: ['', Validators.required],
+      tipoPeriodo: [tipoPeriodo.mes, Validators.required]
+    }),
+    juros: this.formBuilder.group({
+      valor: ['', Validators.required],
+      tipoPeriodo: [tipoPeriodo.mes, Validators.required]
+    }),
+    aportes: this.formBuilder.array([
+      this.formBuilder.group({
+        valor: ['', Validators.required],
+        mes: [mes.todos, Validators.required]
+      })
+    ])
   });
 
+  constructor(private formBuilder: FormBuilder) { }
+
   onSubmit() {
-    // TODO: Use EventEmitter with form value
     console.warn(this.form.value);
+  }
+
+  get aportes() {
+    return this.form.get('aportes') as FormArray;
+  }
+
+  addAportes() {
+    this.aportes.push(this.formBuilder.group({
+      valor: ['', Validators.required],
+      mes: [mes.todos, Validators.required]
+    }));
+  }
+
+  removeAportes(index: number) {
+    this.aportes.removeAt(index);
   }
 }
